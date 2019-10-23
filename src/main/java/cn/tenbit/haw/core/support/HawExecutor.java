c696311f8b65f726baa6cee84091b5125b2d47f0
bangquan.qian <chainz@foxmail.com>
2019-10-23 19:02:47 +0800

    commit

---
+++ src/main/java/cn/tenbit/haw/core/support/HawExecutor.java
@@ -0,0 +1,10 @@
+package cn.tenbit.haw.core.support;
+
+/**
+ * @author bangquan.qian
+ * @date 2019-10-23 19:00
+ */
+public interface HawExecutor<T> {
+
+    T execute();
+}
---
+++ src/main/java/cn/tenbit/haw/core/util/HawTrans.java
@@ -0,0 +1,8 @@
+package cn.tenbit.haw.core.util;
+
+/**
+ * @author bangquan.qian
+ * @date 2019-10-23 18:50
+ */
+public class HawTrans {
+}
